5d42366dc2ec7654061b400d12941b11d6403e46
Simen Graaten <simeng@gmail.com>
2023-03-24 23:19:02 +0100

    Initial dap setup

---
+++ lua/plugins/dap.lua
@@ -0,0 +1,12 @@
+return {
+  {
+    "rcarriga/nvim-dap-ui",
+    requires = { "mfussenegger/nvim-dap" },
+    config = function()
+      require("dapui").setup()
+    end,
+  },
+  {
+    "mfussenegger/nvim-dap",
+  },
+}

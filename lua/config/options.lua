784f41f7eeeac750f6fa6a9502da30e86f8a007a
Simen Graaten <simen@changers.no>
2024-02-04 23:37:51 +0100

    Add wsl config

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -6,3 +6,16 @@ vim.opt.relativenumber = false
 -- vim.opt.clipboard = ""
 vim.opt.conceallevel = 0
 vim.opt.wrap = true
+
+vim.g.clipboard = {
+  name = "WslClipboard",
+  copy = {
+    ["+"] = "clip.exe",
+    ["*"] = "clip.exe",
+  },
+  paste = {
+    ["+"] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+    ["*"] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+  },
+  cache_enabled = 0,
+}

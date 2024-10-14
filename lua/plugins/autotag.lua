3820122f930b6600cbffbfcf0da5833f13147112
Simen Graaten <simen@godpris.no>
2024-10-14 12:02:12 +0200

    Fixed autotag opts

--- lua/plugins/autotag.lua
+++ lua/plugins/autotag.lua
@@ -1,7 +1,7 @@
 return {
-  {
-    "windwp/nvim-ts-autotag",
-    enabled = true,
+  "windwp/nvim-ts-autotag",
+  enabled = true,
+  opts = {
     opts = {
       enable_close = false,
       enable_close_on_slash = false,
